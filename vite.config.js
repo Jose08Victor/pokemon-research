658234c54bee126dca2864d364742603f027c2ad
José Victor <jose08.victor@gmail.com>
2023-12-09 14:29:46 -0300

    added configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "quest-react-avancado"
+  base: "quest-react-avancado",
+  build: {
+    assetsInlineLimit: '500'
+  }
 })
