1934418387a6862fff9f0e104fbc9bce13b398af
José Victor <jose08.victor@gmail.com>
2023-12-10 19:15:59 -0300

    refactored code.

--- src/services/pokemonResource.jsx
+++ src/services/pokemonResource.jsx
@@ -1,4 +1,4 @@
-export async function getPokemons() {
+export async function getFirstGenerationPokemons() {
     let capturedPokemons = [];
     for (let i = 1; i <= 151; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
@@ -21,39 +21,35 @@ export async function getType(types) {
 
 export async function getMoves(moves) {
     let movesName = []
-    let generation1Moves = await generation1()
-    let onlyGeneration1Moves = []
+    let firstGenerationMoves = await firstGeneration()
+    let onlyFirstGenerationMoves = []
 
     moves.map((e) => movesName.push(e.move.name))
 
-    generation1Moves.map((e) => {
-        if (movesName.includes(e.name)) onlyGeneration1Moves.push(e.name)
+    firstGenerationMoves.map((e) => {
+        if (movesName.includes(e.name)) onlyFirstGenerationMoves.push(e.name)
     })
 
-    return onlyGeneration1Moves.join(', ');
+    return onlyFirstGenerationMoves.join(', ');
 }
 
-let generation1 = async () => {
+let firstGeneration = async () => {
     const response = await fetch("https://pokeapi.co/api/v2/generation/1");
     let generation = await response.json();
     return generation.moves
 }
 
 export async function getAbilities(abilities) {
-    let abilityName = []
-    let abilityNameWithDescription = []
+    let abilityWihNameAndDescription = []
 
-    abilities.map((e) => abilityName.push(e.ability.name))
-
-    for (let i = 1; i <= 191; i++) {
-        const response = await fetch(`https://pokeapi.co/api/v2/ability/${i}`);
-        const abilities = await response.json();
-
-        if (abilityName.includes(abilities.name)) abilityNameWithDescription.push({
-                                                                                    name: abilities.name,
-                                                                                    description: abilities.effect_entries[1].short_effect
-                                                                                });
+    for (let i = 0; i <= abilities.length - 1; i++) {
+        const response = await fetch(`https://pokeapi.co/api/v2/ability/${abilities[i].ability.name}`);
+        const ability = await response.json();
+        abilityWihNameAndDescription.push({
+                                            name: ability.name,
+                                            description: ability.effect_entries[1].short_effect
+                                        });
     }
 
-    return (abilityNameWithDescription);
+    return (abilityWihNameAndDescription);
 }
